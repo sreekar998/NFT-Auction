8b913e5df05d4bee9c4b209707f8a6ffe4ea3285
sreekar-wdcs <sreekar@webcluesinfotech.com>
2022-06-19 14:50:17 +0530

    commited

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  
+  const MyToken = await hre.ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy();
+
+  await myToken.deployed();
+
+  console.log("Nft deployed to:", myToken.address);
+}
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
